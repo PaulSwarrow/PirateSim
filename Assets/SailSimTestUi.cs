a621a5d2d10b35d9c798e241fc4b30fb0469af48
Pavel <pavel.suvorov@kosmos.games>
2020-09-17 18:35:47 +0300

    sail material

---
+++ Assets/SailSimTestUi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lib.UnityQuickTools.Collections;
+using ShipSystems.Sim;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SailSimTestUi : MonoBehaviour
+{
+    [SerializeField] private SailCloth sail;
+
+    [SerializeField] private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sail.rigs.Foreach(item=>item.progress = slider.value);
+    }
+}
