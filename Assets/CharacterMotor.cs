b69dcae5ac149f3a413b1a852b92067536f85340
Pavel <pavel.suvorov@kosmos.games>
2020-09-22 16:16:34 +0300

    stairs

--- Assets/CharacterMotor.cs
+++ Assets/CharacterMotor.cs
@@ -12,6 +12,7 @@ public class CharacterMotor : MonoBehaviour
     [SerializeField] private PhysicMaterial frictionMaterial;
     [SerializeField] private PhysicMaterial slideMaterial;
     [SerializeField] private float groundRayLength = 0.05f;
+    [SerializeField] private float groundedRayRadius = 0.4f;
 
     private Camera camera;
     private Animator animator;
@@ -25,6 +26,7 @@ public class CharacterMotor : MonoBehaviour
     private Vector3 localForward;
 
     private bool grounded;
+    private Vector3 floorUp;
 
     void Start()
     {
@@ -44,24 +46,31 @@ public class CharacterMotor : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        grounded = Physics.Raycast(
-            new Ray(transform.position + Vector3.up, Vector3.down),
-            out var groundCollision,
-            1 + groundRayLength);
+        var ray = new Ray(transform.position + Vector3.up, Vector3.down);
+        if (Physics.SphereCast(ray, groundedRayRadius, out var hitinfo, 1 + groundRayLength - groundedRayRadius))
+        {
+            grounded = true;
+            floorUp = ray.origin + Vector3.down * hitinfo.distance - hitinfo.point;
+        }
+        else grounded = false;
+
 
         input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        var vector = camera.transform.TransformDirection(input);
-        vector.y = 0;
-        vector.Normalize();
-        vector *= input.magnitude;
 
         collider.material = input.magnitude > 0 ? slideMaterial : frictionMaterial;
 
-        var velocity = floor.GetPointVelocity(body.position);
+        Vector3 velocity;
 
         if (grounded)
         {
-            velocity += vector * speed;
+            var vector = camera.transform.TransformDirection(input);
+            vector.y = 0;
+            vector.Normalize();
+            vector *= input.magnitude;
+            vector = Quaternion.FromToRotation(Vector3.up, floorUp) * vector;
+
+            Debug.DrawRay(transform.position, vector, Color.yellow);
+            velocity = vector * speed +  floor.GetPointVelocity(body.position);
             if (input.magnitude > 0)
             {
                 localForward = floor.transform.InverseTransformDirection(vector);
@@ -69,10 +78,9 @@ public class CharacterMotor : MonoBehaviour
         }
         else
         {
-            velocity += Physics.gravity;
+            velocity = body.velocity + Physics.gravity * Time.fixedDeltaTime;
         }
 
-        Debug.Log(vector + ", " + velocity);
         body.velocity = velocity;
 
 
@@ -84,13 +92,21 @@ public class CharacterMotor : MonoBehaviour
         worldForward.Normalize();
         body.rotation = Quaternion.LookRotation(worldForward, Vector3.up);
 
-        animator.SetFloat(ForwardKey, vector.magnitude, 0, 1);
+        animator.SetFloat(ForwardKey, input.magnitude, 0, 1);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = grounded ? Color.green : Color.red;
         var a = transform.position + Vector3.up;
-        Gizmos.DrawLine(a, a + Vector3.down * (1 + groundRayLength));
+        var ray = new Ray(transform.position + Vector3.up, Vector3.down);
+        Gizmos.DrawWireSphere(a + Vector3.down * (1 + groundRayLength - groundedRayRadius), groundedRayRadius);
+
+        if (grounded)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, floorUp);
+
+        }
     }
 }
